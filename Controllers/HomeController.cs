8085d2f5e31cd55fb87fd540fc9da1fbedfd8602
Gozzissimo <fantini.nico@gmail.com>
2022-05-31 17:51:49 +0200

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,31 +15,33 @@ namespace la_mia_pizzeria_static.Controllers
 
         public IActionResult Index()
         {
-            ViewData["Title"] = "Pizzeria Bella Genova";
+            //INIZIO SEZIONE PIZZE
+            //--------------------------------
+            //ViewData["Title"] = "Pizzeria Bella Genova";
 
-            ViewData["MargheritaTitle"] = "Margherita";
-            ViewData["DiavolaTitle"] = "Diavola";
-            ViewData["CapricciosaTitle"] = "Capricciosa";
-            ViewData["AmericanaTitle"] = "Americana";
-            ViewData["SalsFriaTitle"] = "Salsiccia e Friarielli";
+            //ViewData["MargheritaTitle"] = "Margherita";
+            //ViewData["DiavolaTitle"] = "Diavola";
+            //ViewData["CapricciosaTitle"] = "Capricciosa";
+            //ViewData["AmericanaTitle"] = "Americana";
+            //ViewData["SalsFriaTitle"] = "Salsiccia e Friarielli";
             
-            ViewData["MargheritaDesc"] = "Pomodori, Mozzarella, Basilico, Olio EVO";
-            ViewData["DiavolaDesc"] = "Pomodori, Mozzarella, Salame Piccante, Olio EVO Piccante";
-            ViewData["CapricciosaDesc"] = "Pomodori, Mozzarella, Prosciutto Cotto, Funghi, Olive, Carciofini";
-            ViewData["AmericanaDesc"] = "Pomodori, Mozzarella, Wurstel, Patatine Fritte";
-            ViewData["SalsFriaDesc"] = "Mozzarella, Salsiccia, Friarielli";
+            //ViewData["MargheritaDesc"] = "Pomodori, Mozzarella, Basilico, Olio EVO";
+            //ViewData["DiavolaDesc"] = "Pomodori, Mozzarella, Salame Piccante, Olio EVO Piccante";
+            //ViewData["CapricciosaDesc"] = "Pomodori, Mozzarella, Prosciutto Cotto, Funghi, Olive, Carciofini";
+            //ViewData["AmericanaDesc"] = "Pomodori, Mozzarella, Wurstel, Patatine Fritte";
+            //ViewData["SalsFriaDesc"] = "Mozzarella, Salsiccia, Friarielli";
             
-            ViewData["MargheritaPrezzo"] = "4.50";
-            ViewData["DiavolaPrezzo"] = "5.00";
-            ViewData["CapricciosaPrezzo"] = "6.50";
-            ViewData["AmericanaPrezzo"] = "7.50";
-            ViewData["SalsFriaPrezzo"] = "7.00";
+            //ViewData["MargheritaPrezzo"] = "4.50";
+            //ViewData["DiavolaPrezzo"] = "5.00";
+            //ViewData["CapricciosaPrezzo"] = "6.50";
+            //ViewData["AmericanaPrezzo"] = "7.50";
+            //ViewData["SalsFriaPrezzo"] = "7.00";
             
-            ViewData["MargheritaImg"] = "img\\margherita.jpg";
-            ViewData["DiavolaImg"] = "img\\diavola.jpg";
-            ViewData["CapricciosaImg"] = "img\\capricciosa.jpg";
-            ViewData["AmericanaImg"] = "img\\americana.jpg";
-            ViewData["SalsFriaImg"] = "img\\salsiccia-friarielli.jpg";
+            //ViewData["MargheritaImg"] = "img\\margherita.jpg";
+            //ViewData["DiavolaImg"] = "img\\diavola.jpg";
+            //ViewData["CapricciosaImg"] = "img\\capricciosa.jpg";
+            //ViewData["AmericanaImg"] = "img\\americana.jpg";
+            //ViewData["SalsFriaImg"] = "img\\salsiccia-friarielli.jpg";
 
             return View();
         }
--- Program.cs
+++ Program.cs
@@ -39,3 +39,23 @@ Modifichiamo quindi la view Index.cshtml generata in automatico da .Net Core scr
 (possiamo creare una tabella con bootstrap o una qualche grafica a nostro piacimento che mostri l'elenco delle pizze...proviamo un po' di creativit‡ se vogliamo!)
 Piccolo suggerimento : ricordatevi di inserire il seguente codice all'inizio della vostra view @ { Layout = null; }
 */
+
+/*
+1. Dichiarare una variabile valorizzandola con il nostro nome.
+Dichiarare un'altra variabile valorizzandola con il nostro cognome.
+Stampare il nostro nome e cognome all'interno di un tag h2.
+
+2. Creare una funzione che stampa la somma dei numeri che vanno da 1 a n (con n passato come parametro - n incluso)
+Stampare dentro uno span il risultato.
+
+3. Contare quante volte la lettera a A Ë presente nalla frase "nel mezzo del cammin di nostra vita" e stampare questo valore in pagina
+
+4. Creare una List<int> di 20 numeri interi (generati random da 1 a 90, 1 e 90 inclusi) e mostrare in pagina solo i numeri compresi tra 20 e 70
+
+5. Stampare in pagina la data e l'ora attuale (usando la classe DateTime) con il formato anno-mese-giorno ore:minuti:secondi
+
+6. Partendo dalla data di oggi (usando la classe DateTime) stampare in pagina
+a. la data di 7 giorni fa
+b. che giorno della settimana √® oggi (il risultato dovr√  essere il nome del giorno (Luned√¨, Marted√¨, ...)
+c. che giorno della settimana sar√  nello stesso giorno e mese di oggi ma tra un anno (il risultato dovr√  essere il nome del giorno (Luned√¨, Marted√¨, ...)
+*/
