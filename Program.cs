a4d7628ac1349810f4f9d164ab19f667ba720861
Gozzissimo <fantini.nico@gmail.com>
2022-05-31 15:25:29 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+/*
+nome repo: la-mia-pizzeria-static
+Ciao ragazzi oggi iniziamo a muovere i primi passi con questo fantastico Asp .Net Core MVC!
+Dobbiamo realizzare un'applicazione web che ci aiuti a gestire la nostra pizzeria.
+Abbiamo bisogno di creare la nostra prima pagina (index) dove mostriamo tutte le pizze che prepariamo.
+
+Una pizza avrà le seguenti informazioni :
+- un nome
+- una descrizione
+- una foto
+- un prezzo
+
+Modifichiamo quindi la view Index.cshtml generata in automatico da .Net Core scrivendo l'html che serve a noi per mostrare l'elenco delle pizze
+(possiamo creare una tabella con bootstrap o una qualche grafica a nostro piacimento che mostri l'elenco delle pizze...proviamo un po' di creatività se vogliamo!)
+Piccolo suggerimento : ricordatevi di inserire il seguente codice all'inizio della vostra view @ { Layout = null; }
+*/
